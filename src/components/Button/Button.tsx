1f7a30290bf8674610fced7c23a43fcb5adb1e10
Gustavo Szilagyi <gustavo.oliveira@tgid.com.br>
2025-08-07 16:24:04 -0300

    primeiro commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react"
+
+export const Button = () =>{
+    const [ counter, setCounter ] = useState<number>(0)
+
+    const increment = () =>{
+        setCounter(counter + 1)
+    }
+
+    return(
+        <button onClick={increment}>{counter}</button>
+    )
+}
